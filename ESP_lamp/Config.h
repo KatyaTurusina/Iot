f3f2ea8e67fb1098221d8f9d911160d7702cffca
Екатерина <130843083+KatyaTurusina@users.noreply.github.com>
2023-10-31 23:58:33 +0800

    Create Config.h

---
+++ ESP_lamp/Config.h
@@ -0,0 +1,8 @@
+String ssid_AP = "ESPWIFI_DASHA";
+String ssid_pass = "ESP11111"; 
+const int LED_PIN = LED_BUILTIN;
+const int mqtt_port = 1883;
+char* mqtt_broker = "broker.emqx.io";
+
+//char* ssid_client = "BestOFtheBest"; 
+//char* pass_clien = "321123321"; 
